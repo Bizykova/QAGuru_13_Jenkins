c50650c7feb6dad7209c232e9036449da8d8fb86
Bizykova <bizykova.daria@gmail.com>
2023-12-20 12:49:52 +0300

    REMOTE_DRIVER_URl change

--- src/test/java/lesson_eight/TestBase.java
+++ src/test/java/lesson_eight/TestBase.java
@@ -26,7 +26,8 @@ public class TestBase {
             System.out.format("%s=%s%n", envName, env.get(envName));
         }
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+       // Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@"+env.get("REMOTE_DRIVER_URL")+"/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
